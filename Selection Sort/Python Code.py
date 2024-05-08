1a06e86b2f1e2d3cccc85b03486fc4193a9d6c70
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-05-08 21:04:46 +0600

    Python Code.py

---
+++ Selection Sort/Python Code.py
@@ -0,0 +1,14 @@
+def selectionSort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+arr = [64, 25, 12, 22, 11]
+selectionSort(arr)
+print("Sorted array:")
+for i in range(len(arr)):
+    print(arr[i], end=" ")
